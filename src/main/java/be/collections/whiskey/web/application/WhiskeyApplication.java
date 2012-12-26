87462d621c7940aedceb5d8fffbc3effe73b3d01
geluykba <bart.geluykens@hp.com>
2012-12-26 21:35:46 +0100

    Don't like java-code and html-logic in the same directory, so I split it up

--- src/main/java/be/collections/whiskey/web/application/WhiskeyApplication.java
+++ src/main/java/be/collections/whiskey/web/application/WhiskeyApplication.java
@@ -4,7 +4,9 @@ import be.collections.whiskey.web.page.DetailsPage;
 import be.collections.whiskey.web.page.EditBrewery;
 import be.collections.whiskey.web.page.EditWhiskey;
 import be.collections.whiskey.web.page.HomePage;
+import org.apache.wicket.core.util.file.WebApplicationPath;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.settings.IResourceSettings;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 /**
@@ -17,6 +19,12 @@ public class WhiskeyApplication extends WebApplication {
   public WhiskeyApplication() {
 
   }
+  private void configureAlternateHtmlLocation() {
+ 		IResourceSettings resourceSettings = getResourceSettings();
+    resourceSettings.getResourceFinders().add(new WebApplicationPath(getServletContext(), ""));
+    resourceSettings.getResourceFinders().add(new WebApplicationPath(getServletContext(), "WEB-INF/views/"));
+ 	}
+
 
   @Override
   public Class getHomePage() {
@@ -26,6 +34,7 @@ public class WhiskeyApplication extends WebApplication {
   @Override
   protected void init() {
     super.init();
+    configureAlternateHtmlLocation();
 
     getComponentInstantiationListeners().add(new SpringComponentInjector(this));
 
